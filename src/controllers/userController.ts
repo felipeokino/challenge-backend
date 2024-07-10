288e4a043133cf708c3322e3e6648a25f4e11243
Felipe Okino <felipe1okino@live.com>
2024-07-10 04:49:17 -0300

    feat: CRUD product

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/User';
+import User from '../models/UserModel';
 
 export const userController = {
   async store(req: Request, res: Response) {
--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,20 @@ import './database';
 dotenv.config();
 
 const app = express();
+const apiVersion = '/v1';
+const apiPrefix = `/api${apiVersion}`;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/api', require('./routes/challenge'));
-app.use('/api/users', require('./routes/user'));
-app.use('/api', require('./routes/auth'));
-app.use('/', require('./routes/challenge'));
+app.use(`${apiPrefix}/users`, require('./routes/user'));
+app.use(apiPrefix, require('./routes/auth'));
+app.use('/', require('./routes/status'));
+app.use(`${apiPrefix}/products`, require('./routes/products'));
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-module.exports = app;
+export default app;
